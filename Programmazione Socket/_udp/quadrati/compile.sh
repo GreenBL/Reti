3c212e42676dc32a05ecb98ffb0d5279aa29133f
GreenBL <Antonino117@hotmail.it>
2023-12-12 10:29:29 +0100

    udp socket

---
+++ Programmazione Socket/_udp/quadrati/compile.sh
@@ -0,0 +1,2 @@
+gcc sqr_udp_client.c -o sqr_c | gcc sqr_udp_server.c -o sqr_s
+echo "GCC: Compilation complete"
---
+++ Programmazione Socket/_udp/quadrati/squarerootsum/compile.sh
@@ -0,0 +1,2 @@
+gcc UDP_server_pow.c -o pow_server | gcc UDP_client_pow.c -o pow_client
+echo "Operation Completed"
